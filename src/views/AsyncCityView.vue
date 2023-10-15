627812e59b18086225f39b0bf09e3da3a6400363
Yusuf Soliman <ahmedmanseah@gmail.com>
2023-10-15 21:07:23 +0200

    developing the city weather

--- src/views/AsyncCityView.vue
+++ src/views/AsyncCityView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div></div>
+  <div class="flex flex-col flex-1 items-center">
+    <div
+      v-if="route.query.preview"
+      class="text-white p-4 bg-sky-700 w-full text-center"
+    >
+      <p>
+        You are currently previewing this city, click the "+" icon to start
+        tracking this city.
+      </p>
+    </div>
+    <!-- city preview -->
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ route.params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString("en-us", {
+            weekday: "short",
+            day: "2-digit",
+            month: "long",
+          })
+        }}
+        {{
+          new Date(weatherData.currentTime).toLocaleTimeString("en-us", {
+            timeStyle: "short",
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">
+        {{ Math.round(weatherData.current.temp) }}&deg;
+      </p>
+      <p>
+        Feels like
+        {{ Math.round(weatherData.current.feels_like) }} &deg;
+      </p>
+      <p class="capitalize">
+        {{ weatherData.current.weather[0].description }}
+      </p>
+      <img
+        class="w-[150px] h-auto"
+        :src="`http://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`"
+        alt=""
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -11,7 +54,7 @@ const route = useRoute();
 const getWeatherData = async () => {
   try {
     const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=metric`
     );
     // cal current date & time
     const localOffset = new Date().getTimezoneOffset() * 60000;
@@ -24,11 +67,10 @@ const getWeatherData = async () => {
       const utc = hour.dt * 1000 + localOffset;
       hour.currentTime = utc + 1000 * weatherData.data.timezone_offset;
     });
-    return weatherData;
+    return weatherData.data;
   } catch (err) {
     console.error(err);
   }
 };
 const weatherData = await getWeatherData();
-console.log(weatherData);
 </script>
