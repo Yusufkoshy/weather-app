5aab1bf8739626dcbda30734b40f0c9e362d9ffe
Yusuf Soliman <ahmedmanseah@gmail.com>
2023-10-16 21:17:16 +0200

    created the hourly weather and weekly forcast

--- src/views/AsyncCityView.vue
+++ src/views/AsyncCityView.vue
@@ -42,6 +42,64 @@
         alt=""
       />
     </div>
+
+    <hr class="border-white border-opacity-10 border w-full" />
+
+    <!-- Hourly preview -->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4 text-xl">Hourly Weather</h2>
+        <div class="flex gap-10 overflow-x-scroll">
+          <div
+            v-for="hour in weatherData.hourly"
+            :key="hour.dt"
+            class="flex flex-col gap-4 items-center"
+          >
+            <p class="whitespace-nowrap text-md">
+              {{
+                new Date(hour.currentTime).toLocaleTimeString("en-us", {
+                  hour: "numeric",
+                })
+              }}
+            </p>
+            <img
+              class="w-auto h-[50px] object-cover"
+              :src="`http://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`"
+              alt=""
+            />
+            <p class="text-xl">{{ Math.round(hour.temp) }}&deg;</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Weekly Preview-->
+    <div class="max-w-screen-md w-full py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4 text-xl">Weekly Forcast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("en-us", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] object-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}</p>
+            <p>L: {{ Math.round(day.temp.min) }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -74,3 +132,24 @@ const getWeatherData = async () => {
 };
 const weatherData = await getWeatherData();
 </script>
+<style scoped>
+/* Start of scrollbar style*/
+
+::-webkit-scrollbar {
+  width: 20px;
+}
+
+::-webkit-scrollbar-track {
+  background: #ddd;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgb(0, 132, 255);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: rgb(0, 47, 255);
+}
+
+/* End of scrollbar style*/
+</style>
